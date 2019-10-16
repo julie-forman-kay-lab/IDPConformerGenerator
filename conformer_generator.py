d38dc2f6b20f313d905945c8332a161db5c0766e
Alaa Ahmed <alaaa.ahmed@mail.utoronto.ca>
2019-10-16 09:46:31 -0400

    Fixed arithmetic error in algorithm

--- conformer_generator.py
+++ conformer_generator.py
@@ -22,7 +22,7 @@ class ProteinSearch:
         """ search every sequential combination of
          sequences in pattern, inside of word """
 
-        def recursive_search(pattern_index, pattern_size, mismatch_num):
+        def recursive_search(pattern_index, pattern_size):
             """ recursively increase bracket_size
              and search through the primary_seq_db"""
 
@@ -32,12 +32,12 @@ class ProteinSearch:
             
             current_pattern = \
                 input_pattern[pattern_index:pattern_index + pattern_size]
-            
+
             previous_pattern = current_pattern[:-1]
 
             # optimization using dynamic programming
             if current_pattern in result:
-                recursive_search(pattern_index, pattern_size + 1, mismatch_num)
+                recursive_search(pattern_index, pattern_size + 1)
 
             for index, prev_mismatch_num in result[previous_pattern]:
 
@@ -46,23 +46,23 @@ class ProteinSearch:
                 except IndexError:  # index is at the end of the sequence
                     continue
                 
-                mismatch = math.ceil(((prev_mismatch_num + mismatch_num + 1) / (pattern_size)) * 100)
+                mismatch = math.ceil(((prev_mismatch_num + 1) / (pattern_size)) * 100)
 
                 # check if the characters are the same, if they're not make sure we're still
                 # under the max_mismatch specified
                 if current_pattern[-1] == character or mismatch <= max_mismatch:
 
                     same_char_bool = 0 if current_pattern[-1] == character else 1
-                    new_mismatch = mismatch_num + prev_mismatch_num
+                    new_mismatch = prev_mismatch_num+same_char_bool
                     new_result = result[current_pattern]
                     if not new_result or index not in list(zip(*new_result))[0]:
-                        result[current_pattern].append((index, new_mismatch+same_char_bool))
+                        result[current_pattern].append((index, new_mismatch))
 
-                    # have we JUST reached the max?
-                    if mismatch == max_mismatch and new_mismatch+same_char_bool != new_mismatch:
+                    # have we JUST reached the max because we found a mismatch?
+                    if mismatch == max_mismatch and new_mismatch != prev_mismatch_num:
                         continue
 
-                    recursive_search(pattern_index, pattern_size + 1, new_mismatch+same_char_bool)
+                    recursive_search(pattern_index, pattern_size + 1)
 
         try:
             primary_seq_db_length = len(primary_seq_db)
@@ -99,7 +99,7 @@ class ProteinSearch:
                 if minimum_sequence not in self.results:
                     result[minimum_sequence] = [(index, 0) for index in indices] #initiallize all to 0 mismatches
                 
-                recursive_search(bracket_index, min_seq_chunk_size + 1, 0)
+                recursive_search(bracket_index, min_seq_chunk_size + 1)
 
         self.proteins_sequences.append(primary_seq_db)
         self.results.append(result)
@@ -145,54 +145,52 @@ class TestSearch:
 
 # -------------- FOR MANUAL TESTING ONLY USE BELOW --------------------
 
-def string_generator():
-    string = "GAYHLRHEVKEDMALAAAAAAAPNSLWWITVICIHPCTRWNKTAPEMAQVWWWGVQERMFAWSPMLLLKFFAPLFKGLCFTMRAQPGRAMKQNLVDGEWTRKYVCIRDRKSSNHRSWVSMFNYQLMCSEGATSIRIRPFHWQWPYEWMYHDNYHKSWWLSVEEWCHWFNRNDNGSLNHTMPCRDYCINDASHHKIWHGVILATTTTTTKIHEWHMSVNAWMGLAKLAIMLHHFSNVMGLTQMETLCQHATHEIMFVMWVIDFMNGPAQQHDKDFEDVSEYPKLFQNDPVSPEQYWIGEPDDQLKYTYDITVVFAMNDNMCVTGNPDHIVHHHFKNWANHVNHNPFEQNDDDASYGIKHEQWCYNVDMDVHKGWRWPSFKKYYTTNAEAPNFSDNFISEDQMLEELVDIQD"
-    randomize = "GAYHLRHEVKEDMNSLWWITVICIHPCTRWNKTAPEMAQVWWWGVQERMFAWSPMLLLKFFAPLFKGLCFTMRAQPGRAMKQNLVDGEWTRKYVCIRDRKSSNHRSWVSMFNYQLMCSEGATSIRIRPFHWQWPYEWMYHDNYHKSWWLSVEEWCHWFNRNDNGSLNHTMPCRDYCINDASHHKIWHGVILATTTTTTKIHEWHMSVNAWMGLAKLAIMLHHFSNVMGLTQMETLCQHATHEIMFVMWVIDFMNGPAQQHDKDFEDVSEYPKLFQNDPVSPEQYWIGEPDDQLKYTYDITVVFAMNDNMCVTGNPDHIVHHHFKNWANHVNHNPFEQNDDDASYGIKHEQWCYNVDMDVHKGWRWPSFKKYYTTNAEAPNFSDNFISEDQMLEELVDIQD"
-    for _ in range(28000):
-        string+=(''.join(random.sample(randomize,len(randomize))))
-    return string
+# def string_generator():
+#     string = "GAYHLRHEVKEDMALAAAAAAAPNSLWWITVICIHPCTRWNKTAPEMAQVWWWGVQERMFAWSPMLLLKFFAPLFKGLCFTMRAQPGRAMKQNLVDGEWTRKYVCIRDRKSSNHRSWVSMFNYQLMCSEGATSIRIRPFHWQWPYEWMYHDNYHKSWWLSVEEWCHWFNRNDNGSLNHTMPCRDYCINDASHHKIWHGVILATTTTTTKIHEWHMSVNAWMGLAKLAIMLHHFSNVMGLTQMETLCQHATHEIMFVMWVIDFMNGPAQQHDKDFEDVSEYPKLFQNDPVSPEQYWIGEPDDQLKYTYDITVVFAMNDNMCVTGNPDHIVHHHFKNWANHVNHNPFEQNDDDASYGIKHEQWCYNVDMDVHKGWRWPSFKKYYTTNAEAPNFSDNFISEDQMLEELVDIQD"
+#     randomize = "GAYHLRHEVKEDMNSLWWITVICIHPCTRWNKTAPEMAQVWWWGVQERMFAWSPMLLLKFFAPLFKGLCFTMRAQPGRAMKQNLVDGEWTRKYVCIRDRKSSNHRSWVSMFNYQLMCSEGATSIRIRPFHWQWPYEWMYHDNYHKSWWLSVEEWCHWFNRNDNGSLNHTMPCRDYCINDASHHKIWHGVILATTTTTTKIHEWHMSVNAWMGLAKLAIMLHHFSNVMGLTQMETLCQHATHEIMFVMWVIDFMNGPAQQHDKDFEDVSEYPKLFQNDPVSPEQYWIGEPDDQLKYTYDITVVFAMNDNMCVTGNPDHIVHHHFKNWANHVNHNPFEQNDDDASYGIKHEQWCYNVDMDVHKGWRWPSFKKYYTTNAEAPNFSDNFISEDQMLEELVDIQD"
+#     for _ in range(28000):
+#         string+=(''.join(random.sample(randomize,len(randomize))))
+#     return string
 
-def stopWatch(value):
-    '''From seconds to Days;Hours:Minutes;Seconds'''
+# def stopWatch(value):
+#     '''From seconds to Days;Hours:Minutes;Seconds'''
 
-    valueD = (((value/365)/24)/60)
-    Days = int (valueD)
+#     valueD = (((value/365)/24)/60)
+#     Days = int (valueD)
 
-    valueH = (valueD-Days)*365
-    Hours = int(valueH)
+#     valueH = (valueD-Days)*365
+#     Hours = int(valueH)
 
-    valueM = (valueH - Hours)*24
-    Minutes = int(valueM)
+#     valueM = (valueH - Hours)*24
+#     Minutes = int(valueM)
 
-    valueS = (valueM - Minutes)*60
-    Seconds = int(valueS)
-    print(Hours, " Hours", Minutes, " Minutes" ,Seconds, " seconds")
+#     valueS = (valueM - Minutes)*60
+#     Seconds = int(valueS)
+#     print(Hours, " Hours", Minutes, " Minutes" ,Seconds, " seconds")
 
-if __name__ == '__main__':
-    # 400 residues
-    input_seq = "YCSMLILDQNWALAAAAAAPSEGSSLRCCGCSNMMLFASFMMCKEYHQQYNWREWKSTNTACCHMWHISKLHGYRVQLDKCDLYHQDDDRTDRYWDIINLADEEIRSRGSPDQKSWCQSMGRYQVWSMQSAWGCLEPSPRKLMEPPYYRGMSKWEKDSKYSNRTMFAPCADRESRYWEMYQRQKIEPKRLDFAQTCRWLTVASRIFTQWICWWPKKQDMPVKISLQMGKGGISEWAVLSALFGQGMNQVKGKMPVKQYCKYNCGWEAYNKSPTWVVKGMAMMQGLTRTGSNDYGHAMWDTLKWEAVFCFRQTRQWRWWIECIGKYWHYWVFYWDVQRLAVTLTRYGRRDYEPGAGWSNQDAVINRFDAYHATFYPAIGEYPSLGGGWAMNQDRQWWQMCFSSCPQGHEM"
+# if __name__ == '__main__':
 
-    inputt = "GAYHLRHEVKEDMAMGCIKSKGKDSLSDDGVDLKTQPVRNTERTIYVRDPTSNKQQRPVPESQLLPGQRFQTKDPE"
+#     input_seq = "GAYHLRHEVKEDMAMGCIKSKGKDSLSDDGVDLKTQPVRNTERTIYVRDPTSNKQQRPVPESQLLPGQRFQTKDPE"
 
-    print("generating random sequence..")
-    start = time.time()
-    primary_seq_db = string_generator()
-    middle = time.time()
-    stopWatch(middle-start)
+#     print("generating random sequence..")
+#     start = time.time()
+#     primary_seq_db = string_generator()
+#     middle = time.time()
+#     stopWatch(middle-start)
 
-    search = ProteinSearch()
-    print("searching..")
-    search.start_search(inputt, primary_seq_db, 5, 10)
-    end = time.time()
-    stopWatch(end-middle)
-    print("done")
-    print(len(search.results[-1]))
-    search.start_search(inputt, primary_seq_db, 5, 0)
-    print(len(search.results[-1]))
+#     search = ProteinSearch()
+#     print("searching..")
+#     search.start_search(input_seq, primary_seq_db, 5, 25)
+#     end = time.time()
+#     stopWatch(end-middle)
+#     print("done")
+#     print(len(search.results[-1]))
+#     search.start_search(input_seq, primary_seq_db, 5, 0)
+#     print(len(search.results[-1]))
 
-    print("\ndifference:\n")
-    value = { k : search.results[-1][k] for k in set(search.results[-1]) - set(search.results[-2]) }
-    print(value)
+    # print("\ndifference:\n")
+    # value = { k : search.results[-2][k] for k in set(search.results[-2]) - set(search.results[-1]) }
+    # print(value)
 
 
     
