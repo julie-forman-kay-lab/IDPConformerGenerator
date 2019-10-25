48bdaeb9f25d22ed124b9be864a7ab70e1086207
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-10-25 10:34:34 -0400

    completed exceptions

--- src/idpconfgen/core/exceptions.py
+++ src/idpconfgen/core/exceptions.py
@@ -1,14 +1,37 @@
 """IDP Conf Gen Exceptions."""
+from idpconfgen import contactus as CONTACTUS
 
 
-class PDBIDFactoryError(Exception):
+class IDPConfGenException(Exception):
+    """Base class exception for IDPConfGen."""
+
+    errmsg = 'The following error as occurred: {}.' + CONTACTUS.contact_message
+
+    def __init__(self, *args):
+        self.args = args or ['']
+
+    def __str__(self):
+        """Represent exception as string."""
+        return self.errmsg.format(*self.args)
+
+    def __repr(self):
+        """Represent exception."""
+        return f'{self.__class__.__name__}: {self}'
+
+
+class PDBIDFactoryError(IDPConfGenException):
+    """General PDBIDFactory Exception."""
+
     pass
 
 
-class DownloadFailedError(Exception):
+class DownloadFailedError(IDPConfGenException):
+    """Raise when download fails."""
+
     pass
 
 
-class EmptyFilterError(Exception):
-    pass
+class EmptyFilterError(IDPConfGenException):
+    """Raise when PDB data filtering returns an empty selection."""
 
+    pass
