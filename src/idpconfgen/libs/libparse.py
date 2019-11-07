f53cf1aff5259614a9f64f0aeb347caf10427274
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-11-07 16:32:02 -0500

    DSSP Parser with tests"

--- src/idpconfgen/libs/libparse.py
+++ src/idpconfgen/libs/libparse.py
@@ -1,7 +1,11 @@
 """
 Contain different parsing strategies for different files.
 """
+import numpy as np
+
 from idpconfgen import Path, log
+from idpconfgen.core import exceptions as EXCPTS
+from idpconfgen.core import definitions as DEFS
 
 
 class DSSPParser:
@@ -13,6 +17,8 @@ class DSSPParser:
     
     def __init__(self, *, fin=None, data=None, pdbid=None):
         
+        self.pdbid = pdbid
+        
         if fin and Path(fin).exists():
             self.read_dssp_data(Path(fin).read_text())
         elif data:
@@ -20,9 +26,95 @@ class DSSPParser:
         else:
             self.data = None
 
-        self.pdbid = None
-
     def read_dssp_data(self, data):
-        self.data = data
         
+        try:
+            data = data.split('\n')
+        except AttributeError:  # data already a list
+            pass
+        
+        data = [i for i in data if i]  # removes empty strings
+       
+        try:
+            data_header_index = self._finds_data_index(data)
+        except IndexError as err:
+            raise EXCPTS.DSSPParserError(self.pdbid) from err
+            
+        self.data = data[data_header_index  + 1:]  # data starts afterthe header
 
+        #self.data = self._finds_data_index(data)
+        self.read_sec_structure()
+
+    def read_sec_structure(self):
+        """
+        Read secondary structure information from DSSP files.
+        
+        Assigns :attr:`ss`.
+        """
+        self.ss = list_index_to_array(self.data, index=16)
+        self._confirm_ss_data(self.ss)
+
+    @staticmethod
+    def _confirm_ss_data(data):
+        # confirms data makes sense
+        if not all((i in vars(DEFS.dssp_ss_keys).values() for i in data)):
+            raise EXCPTS.DSSPSecStructError(data)
+
+    @staticmethod
+    def _finds_data_index(data):
+        """
+        Find index for where item startswith '#'.
+
+        Evaluates after left striping white spaces.
+        """
+        # brute force index finding
+        i = 0
+        line = data[i]
+        # if not exausted, while breaks with IndexError
+        while not line.lstrip().startswith('#'):
+            i += 1
+            line = data[i]
+        else:
+            return i
+
+
+def list_index_to_array(
+        list_,
+        index=None,
+        sObj=None,
+        start=None,
+        stop=None,
+        step=None,
+        ):
+    """
+    Extract slices of strings in lists to an array.
+    """
+     
+    if any((i is not None for i in (start, stop, step))):
+        index = slice(start, stop, step)
+    
+    elif sObj is not None:
+        if isinstance(sObj, slice):
+            index = sObj
+        else:
+            raise ValueError(
+                'sObj must be slice type: {} given.'.format(type(sObj))
+                )
+
+    elif index is not None:
+        index = index
+
+    else:
+        raise ValueError('At least one of the index args most be provided.')
+
+    len_of_slice = len(list_[0][index])
+
+    array = np.empty(
+        len(list_),
+        dtype='<U{}'.format(len_of_slice),
+        )
+
+    for i, line in enumerate(list_):
+        array[i] = line[index]
+    
+    return array
--- tests/test_libparse.py
+++ tests/test_libparse.py
@@ -1,21 +1,75 @@
+"""
+Test data file parsers.
+
+Parsers are coded in libparse.
+
+Currently tests:
+
+    - DSSPParser
+"""
+import pytest
 
 from idpconfgen import Path
+from idpconfgen.core import exceptions as EXCPTS
+from idpconfgen.core import definitions as DEFS
 from idpconfgen.libs import libparse
+
+from . import tcommons
+
+
+class TestLITA():
+    
+    data = [
+        'i like to program in python',
+        'u like to program in java',
+        'w like to program in csharp',
+        ]
+
+    def test_list_index_to_array_1(self):
+        """Test index."""
+        parsed = libparse.list_index_to_array(self.data, index=0)
+        assert list(parsed) == ['i', 'u', 'w']
+
+    def test_list_index_to_array_2(self):
+        """Test slice."""
+        parsed = libparse.list_index_to_array(self.data, start=21, stop=25)
+        assert list(parsed) == ['pyth', 'java', 'csha']
+
+    def test_list_index_to_array_3(self):
+        """Test ValueError."""
+        with pytest.raises(ValueError):
+            libparse.list_index_to_array(self.data)
+   
+    def test_list_index_to_array_4(self):
+        """Test IndexError."""
+        with pytest.raises(IndexError):
+            libparse.list_index_to_array(self.data, index=400)
+
+    def test_list_index_to_array5(self):
+        """Test slice object."""
+        parsed = libparse.list_index_to_array(self.data, sObj=slice(21, 25, 1))
+        assert list(parsed) == ['pyth', 'java', 'csha']
+    
+    def test_list_index_to_array6(self):
+        """Test slice object."""
+        with pytest.raises(ValueError):
+            libparse.list_index_to_array(self.data, sObj=5)
 
 
 class TestDSSPParser:
-
-    dssp_file = Path(
-        Path(__file__).myparents(),
-        'data',
-        '1A12_A.dssp',
-        )
-    
+    """Test DSSPParser."""
+    dssp_file = tcommons.data_folder / '1ABC_D.dssp'
+    dssp_wrong = tcommons.data_folder / 'wrong.dssp'
     dsspdata = dssp_file.read_text()
 
     def test_dssp1(self):
         """Test initiation empty args.""" 
         libparse.DSSPParser()
+    
+    def test_dssp1_1(self):
+        """Test pdbid attribute.""" 
+        dobj = libparse.DSSPParser(pdbid='1ABC')
+        assert dobj.pdbid == '1ABC'
 
     def test_dssp2(self):
         """Test initiation from data."""
@@ -25,4 +79,20 @@ class TestDSSPParser:
         """Test initiation from file."""
         libparse.DSSPParser(fin=self.dssp_file)
 
-    
+    def test_dssp4(self):
+        """Test secondary structure parsing."""
+        dobj = libparse.DSSPParser(fin=self.dssp_file)
+
+        expected = [' ', 'H', 'G', 'I', 'B', 'E', ' ', 'T', 'S', ' ']
+        assert list(dobj.ss) == expected
+   
+    def test_dssp5(self):
+        """Test DSSPParserError."""
+        with pytest.raises(EXCPTS.DSSPParserError):
+            dobj = libparse.DSSPParser(fin=self.dssp_wrong)
+
+    def test_dssp6(self):
+        """Test find index."""
+        data = self.dssp_file.read_text().split('\n')
+        index = libparse.DSSPParser._finds_data_index(data)
+        assert index == 28
