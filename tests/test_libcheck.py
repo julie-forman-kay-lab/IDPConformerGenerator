5cbf2c2312d275d96f565ea8e0fca7dc1034c381
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-11-21 15:17:25 -0500

    improved libcheck test pytest.mark

--- tests/test_libcheck.py
+++ tests/test_libcheck.py
@@ -3,7 +3,7 @@ import pytest
 
 from idpconfgen.libs import libcheck
 
-@libcheck.argstype(int, float)
+@libcheck.argstype(int, (float,int))
 @libcheck.kwargstype(str)
 def dummy_function(arg1, arg2, arg3='string'):
     print(arg1, arg2, arg3)
@@ -12,13 +12,31 @@ def dummy_function(arg1, arg2, arg3='string'):
 @pytest.mark.parametrize(
     'arg1,arg2,arg3',
     [
-        ('a', 1.0, 's'),
-        (1, 'a', 's'),
-        (1, 1.0, 1),
-        ([1], ['a'], 's'),
-        (1, 1.0, ['s']),
+        (1.0, 1, 'a'),
+        ([1], 1, 'a'),
+        ('1', 1, 'a'),
+        ((1,), 1, 'a'),
+        (1, [1], 'a'),
+        (1, '1', 'a'),
+        (1, (1,), 'a'),
+        (1, 1, 1),
+        (1, 1, 1.0),
+        (1, 1, ['a']),
         ]
     )
-def test_argstype(arg1, arg2, arg3):
+def test_type_error(arg1, arg2, arg3):
+    """Test type check with TypeError."""
     with pytest.raises(TypeError):
         dummy_function(arg1, arg2, arg3=arg3)
+
+
+@pytest.mark.parametrize(
+    'arg1,arg2,arg3',
+    [
+        (1, 1.0, 'a'),
+        (1, 1, 'a'),
+        ]
+    )
+def test_types_no_erros(arg1, arg2, arg3):
+    """Test args without errors."""
+    dummy_function(arg1, arg2, arg3)
