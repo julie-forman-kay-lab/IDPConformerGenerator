f560d69c474fd6b6eb3a95afe3c8d95ea8aca14d
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-11-27 18:36:20 -0500

    added definitions for NeRF atoms

--- src/idpconfgen/core/definitions.py
+++ src/idpconfgen/core/definitions.py
@@ -3,6 +3,49 @@ from argparse import Namespace
 
 import numpy as np
 
+class Atom:
+    """
+    Defines an atom building block.
+
+    Atoms building blocks are used during the confomer construction.
+    
+    The offset attributes (`_off`) refer to the residues from which
+    the frame atoms will be read according to the Natural Extension
+    Reference Frame building protocol. 0 means the reference atom is taken
+    from the same residue where the Atom is going to the inserted.
+    Likewise, -1 means from the previous residue and 1 from the next
+    residue.
+
+    See :class:`ConformerBuilder`
+
+    Attributes
+    ----------
+    name
+        The name of the atom when built in the conformer.
+
+    parentoff
+        The residue index where the parent atom will be taken.
+
+    xoff
+        The residue index where the xaxis atom wil be taken.
+    
+    yoff
+        The residue index where the yaxis atom wil be taken.
+    """
+    def __init__(self, name, parentoff, xoff, yoff):
+        self.name = name
+        self.poff = parentoff
+        self.xoff = xoff
+        self.yoff = yoff
+    
+    def __repr__(self):
+        kwargs = ', '.join(f'{key}={val!r}' for key, val in self.__dict__.items())
+        rpr = '{}({})'.format(
+            __class__.__name__,
+            kwargs,
+            )
+        return rpr
+
 # keys from https://github.com/cmbi/dssp/blob/7c2942773cd37d47b3e4611597d5e1eb886d95ba/src/dssp.cpp#L66-L74  # noqa:
 dssp_ss_keys = Namespace(
     ahelix='H',
@@ -67,11 +110,16 @@ N_seed = np.array([0.000, 0.000, 0.000], dtype=np.float32)
 CA_seed = np.array([1.458, 0.000, 0.000], dtype=np.float32)
 C_seed = np.array([2.009, 1.420, 0.000], dtype=np.float32)
 
-N_atom = 'N'
-CA_atom = 'CA'
-C_atom = 'C'
-O_atom = 'O'
-COO_atom = 'X'
+N_name = 'N'
+CA_name = 'CA'
+C_name = 'C'
+O_name = 'O'
+COO_name = 'X'
 
-backbone_atoms = (N_atom, CA_atom, C_atom, O_atom)
+N_atom_NeRF = Atom(N_name, -1, -1, -1)
+CA_atom_NeRF = Atom(CA_name, 0, -1, -1)
+C_atom_NeRF = Atom(C_name, 0, 0, -1)
+O_atom_NeRF = Atom(O_name, 0, 0, 1)
+
+backbone_atoms = (N_name, CA_name, C_name, O_name)
 num_bb_atoms = len(backbone_atoms)
