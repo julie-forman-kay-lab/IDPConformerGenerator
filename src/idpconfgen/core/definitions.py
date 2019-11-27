629b21cfd41648b654aa3418c22d8f7cba109166
Joao MC Teixeira <joaomcteixeira@gmail.com>
2019-11-27 11:34:21 -0500

    add aa 3to1 letters

--- src/idpconfgen/core/definitions.py
+++ src/idpconfgen/core/definitions.py
@@ -30,3 +30,26 @@ dssp_ss_keys.all_loops = (
     dssp_ss_keys.bend,
     dssp_ss_keys.loop,
     )
+
+
+aa3to1 = {
+    'ALA': 'A',
+    'ARG': 'R',
+    'ASN': 'N',
+    'ASP': 'D',
+    'CYS': 'C',
+    'GLU': 'E',
+    'GLN': 'Q',
+    'GLY': 'G',
+    'HIS': 'H',
+    'ILE': 'I',
+    'LEU': 'L',
+    'LYS': 'K',
+    'MET': 'M',
+    'PHE': 'F',
+    'PRO': 'P',
+    'SER': 'S',
+    'THR': 'T',
+    'TRP': 'W',
+    'TYR': 'Y',
+    'VAL': 'V'}
