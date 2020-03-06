d3df9f9234f17c9e14a90a6e1e0f8b6a798d3736
Joao MC Teixeira <joaomcteixeira@gmail.com>
2020-03-06 12:52:35 -0500

    Improved Exception engine
    Using the same strategy as for IDPCalculator.

--- src/idpconfgen/core/exceptions.py
+++ src/idpconfgen/core/exceptions.py
@@ -1,22 +1,71 @@
 """IDP Conf Gen Exceptions."""
 from idpconfgen import contactus as CONTACTUS
+from idpconfgen.core import has_string_formatters
 
 
 class IDPConfGenException(Exception):
-    """Base class exception for IDPConfGen."""
+    r"""
+    IDPConfGen base exception.
 
-    errmsg = 'An error as occurred: {}. ' + CONTACTUS.contact_message
+    Example:
 
-    def __init__(self, *args):
-        self.args = args or ['']
+        >>> err = IDPCalculator(var1, var2)
+        
+        >>> err = IDPConfGenException('An error happened: {}, {}', var1, var2)
+        
+        >>> err = IDPConfGenException(errmsg='Custom error msg')
+        
+        >>> err = IDPConfGenException(errmsg='')
 
+    Parameters
+    ----------
+    *args
+        If the first element of args contains ``'{}'`` it will be
+        used as :attr:`errmsg` base string.
+        Else, ``args`` are used to feed the sting method ``.format()``
+        for the default exception :attr:`errmsg`.
+
+    errmsg : optional
+        If given, overrides any previous parameter and the ``str``
+        value of ``errmsg`` is used as the Exception message.
+        Defaults to ``None``.
+    """
+
+    errmsg = 'An unknnown error as occurred. ' + CONTACTUS.contact_message
+
+    def __init__(self, *args, errmsg=None):
+    
+        # require
+        if errmsg:
+            self.errmsg = str(errmsg)
+            self.args = []
+
+        elif args and has_string_formatters(args[0]):
+            self.errmsg = args[0]
+            self.args = args[1:]
+
+        else:
+            self.args = args
+
+        # ensure
+        assert self.errmsg.count('{}') == len(self.args)
+     
     def __str__(self):
-        """Represent exception as string."""
+        """Make me a string :-)."""
         return self.errmsg.format(*self.args)
 
-    def __repr__(self):
-        """Represent exception."""
-        return f'{self.__class__.__name__}: {self}'
+    def report(self):
+        """
+        Report error in the form of a string.
+        
+        Identifies Error type and error message.
+        
+        Returns
+        -------
+        str
+            The formatted string report.
+        """
+        return f'{self.__class__.__name__} * {self}'
 
 
 class PDBIDFactoryError(IDPConfGenException):
@@ -49,7 +98,7 @@ class DSSPSecStructError(IDPConfGenException):
     errmsg = 'Values differ from possible DSSP secondary structure keys.'
 
 
-class DSSPInputMissing(IDPConfGenException):
+class DSSPInputMissingError(IDPConfGenException):
     """Raise when libs.libparse.DSSPParserError needs it."""
 
     errmsg = 'One of the two required positional arguments is missing.'
