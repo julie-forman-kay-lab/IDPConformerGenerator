1cebfbad9b5e245492a92b759950e94b23b05d73
joaomcteixeira <joaomcteixeira@gmail.com>
2020-05-07 22:04:32 -0400

    corrects bug in libdownload

---
+++ tests/test_clis.py
@@ -0,0 +1,32 @@
+"""Test clients."""
+import shutil
+
+from idpconfgen import Path
+from idpconfgen.cli_pdbdownloader import main as pdbdl
+
+from . import tcommons
+
+
+def test_pdbdl_main_1():
+    """Test main from cli_pdbdownloader."""
+    folderout = Path('downloaded_test')
+    folderout.mkdir(exist_ok=True)
+    pdbdl(
+        [tcommons.cull],
+        destination=folderout,
+        update=True,
+        )
+    shutil.rmtree(folderout)
+
+
+def test_pdbdl_main_2():
+    """Test main from cli_pdbdownloader."""
+    folderout = Path('downloaded_test')
+    folderout.mkdir(exist_ok=True)
+    pdbdl(
+        [tcommons.cull],
+        destination=folderout,
+        update=True,
+        ncores=2,
+        )
+    shutil.rmtree(folderout)
