313f0c7b4b747e6bbc6ae157c8c013c61aa726b3
joaomcteixeira <joaomcteixeira@gmail.com>
2020-06-23 19:37:51 -0400

    tests libtimer 80%

--- tests/test_libtimer.py
+++ tests/test_libtimer.py
@@ -3,6 +3,24 @@ from idpconfgen.libs import libtimer
 import pytest
 
 
+#@pytest.mark.parametrize(
+#    'items, cls',
+#    [
+#        (list(range(10)), libtimer.ProgressBar),
+#        (range(10), libtimer.ProgressCounter),
+#        ]
+#    )
+def test_ProgressWatcher():
+    """
+    Test ProgressWatcher constructor.
+
+    Running tests with `tox` the os.get_terminal_size
+    raises IOError, so only ProgressFake is expected.
+    """
+    result = libtimer.ProgressWatcher(range(10))
+    assert isinstance(result, libtimer.ProgressFake)
+
+
 def test_ProgressBar_1():
     """Test progress bar."""
     kw = {'prefix': 'hello', 'suffix': 'frames', 'decimals': 2}
@@ -11,9 +29,6 @@ def test_ProgressBar_1():
         for i in range(40):
             PB.increment()
             assert PB.counter == i + 2
-
-    with pytest.raises(IndexError):
-        PB.increment()
 
 
 def test_ProgressBar_2():
@@ -30,5 +45,15 @@ def test_ProgressBar_2():
             PB.increment()
             assert PB.counter == i + 2
 
-    with pytest.raises(IndexError):
-        PB.increment()
+def test_ProgressFake():
+    """Test ProgressFake interface."""
+    with libtimer.ProgressFake() as PF:
+        PF.increment()
+
+def test_ProgressCounter():
+    """Test ProgressCounter."""
+    with libtimer.ProgressCounter() as PC:
+        assert PC.counter == 0
+        for i in range(10):
+            PC.increment()
+            assert PC.counter == i + 1
