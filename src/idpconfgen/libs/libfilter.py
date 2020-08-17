19022e8c2f20e339bafc5d210eef4eb463f49746
joaomcteixeira <joaomcteixeira@gmail.com>
2020-08-17 17:30:28 +0200

    1st implementation of structured regex search
    Searchs considers:
    * forward with and without repetition.
    * forward and backwards with repetition for reanges
    
    TODO: tests

--- src/idpconfgen/libs/libfilter.py
+++ src/idpconfgen/libs/libfilter.py
@@ -1,9 +1,16 @@
 """Contain functions to filter information from the DB."""
+import itertools as it
 import re
+from functools import partial
 
 import numpy as np
 
-from idpconfgen.core.exceptions import IDPConfGenException
+from idpconfgen.libs.libmulticore import pool_function
+
+
+REGEX_OVERLAP = re.compile(r'\(\?\=\(.+\)')
+REGEX_RANGE = re.compile(r'(\{\d\,\d\}|\{\d\}|\{\d\,\}|\{\,\d\})')
+REGEX_RANGE_CHAR = re.compile(r'\w\{')
 
 
 def aligndb(db, NAN=np.nan):
@@ -60,27 +67,152 @@ def aligndb(db, NAN=np.nan):
 # forward no overlap
 
 
-def regex_search(sequence, regex):
-    """Search for regex in sequence)."""
-    overlap_r = re.compile('\(\?\=\(.+\)')
-    rex = re.compile(regex)
+def regex_search(sequence, regex_string, rex_range=REGEX_RANGE):
+    """
+    Search for regex in sequence.
 
-    if overlap_r.findall(regex):
-        slices = regex_forward_with_overlap(sequence, rex)
+    Return
+    ------
+    list of slices
+    """
+    # if a range exists in regex_string
+    # range is defined by default by: L{1}, L{1,5} situations
+    # the following functions ensures searchs goes both directions
+    if rex_range.findall(regex_string):
+        result = regex_range(sequence, regex_string)
 
     else:
-        # do without overlap
-        slices = regex_forward_no_overlap(sequence, rex)
+        overlap = regex_has_overlap(regex_string)
+        func = regex_forward_with_overlap \
+            if overlap \
+            else regex_forward_no_overlap
+        result = func(sequence, regex_string)
+
+    assert isinstance(result, list)
+    assert all(isinstance(S, slice) for S in result)  # heavy and slow!!
+    return result
+
+
+def regex_has_overlap(regex_string, overlap_fmt=REGEX_OVERLAP):
+    """
+    Find if a `regex_string` defines overlap.
+
+    Parameters
+    ----------
+    regex_string : str
+        The regex string.
+
+    overlap_fmt : str
+        The regex to find overlap in regex_string.
+
+    Returns
+    -------
+    bool
+    """
+    return bool(overlap_fmt.findall(regex_string))
+
+
+def regex_range(sequence, regex_string, ncores=1):
+    """
+    Find slices of sequence where regex_string applies.
+
+    Searches forward and backwards by means of exploring ranges
+    as individual regexes.
+    """
+    # prefix and suffix regex for overlap
+    if regex_has_overlap(regex_string):
+        pre, suf, func = r'(?=(', r'))', regex_forward_with_overlap
+    else:
+        pre, suf = '', '', regex_forward_no_overlap
+
+    # creates all possible regex combinations without ranges
+    # from the rangex identified in the original regex_string
+    regex_combinations = make_regex_combinations(regex_string, pre=pre, suf=suf)
+
+    exec_pool = pool_function(
+        partial(func, sequence),
+        regex_combinations,
+        ncores=ncores,
+        )
+
+    slices = []
+    for result in exec_pool:
+        slices.extend(result)
 
     return slices
 
 
+def make_regex_combinations_from_ranges(regex_string, pre='', suf=''):
+    """."""
+    ranges, chars = make_ranges(regex_string)
+    return make_regex_combinations(ranges, chars, pre=pre, suf=suf)
+
+
+def make_regex_combinations(ranges, chars, pre='', suf=''):
+    """
+    Make combinations of regexes from `ranges` and `chars`.
+
+    This function is not a general abstraction. Is instead a partial
+    abstraction within the problem of IDPConfGen.
+
+    Parameters
+    ----------
+    ranges : list of range objects
+        Ranges where to start and stop searching in the regex.
+        Ranges should follow regex conventions, usually 1-indexed
+        and stop inclusive.
+
+    chars : list of 1 letter chars
+        The chars that will be searched in ranges.
+
+    pre, suf : str
+        Strings to add as prefix and suffix of the generates ranges.
+    """
+    regex_combinations = []
+    for range_tuple in it.product(*ranges):
+        c_regex = (c + str(ii) + '}' for ii, c in zip(range_tuple, chars))
+        regex_combinations.append(f"{pre}{''.join(c_regex)}{suf}")
+    return regex_combinations
+
+
+def make_ranges(
+        regex_string,
+        rang_rex=REGEX_RANGE,
+        char_rex=REGEX_RANGE_CHAR,
+        ):
+    """."""
+    # requires
+    assert isinstance(regex_string, str)
+
+    # examples of i according to `prev`
+    # 'L{', 'H{'
+    chars = [i[:-1] for i in char_rex.findall(regex_string)]
+
+    # yes, I tried to write this in a list comprehension, but these are
+    # 3 for loops. I thought is just more readable to make it explicit
+    rangs = (i.strip('{}') for i in rang_rex.findall(regex_string))
+    ranges = []
+    for trange in rangs:
+        # examples of trange:
+        # '1', '1,', '1,2', ',2'
+        # to make ranges compatible with regex, start should be equal to 1
+        # and because regex ranges are inclusive, 1 must be added to stop
+        values = [int(i) if i else 1 for i in trange.split(',')]
+        ranges.append(range(values[0], values[-1] + 1))
+
+    # ensures
+    assert isinstance(ranges, list)
+    assert isinstance(chars, list)
+    assert len(ranges) == range(chars)
+    return ranges, chars
+
 
 def regex_forward_no_overlap(sequence, regex):
-    """
-    Searches for regex forward without overlap.
+    r"""
+    Search for regex forward without overlap.
 
-    Examples:
+    Examples
+    --------
 
         r'L'
         r'L{2}'
@@ -100,26 +232,24 @@ def regex_forward_no_overlap(sequence, regex):
     # m.span() is used for regexes without overlap
     # using m.start(1) would not work here.
     # See regex_forward_with_overlap
-    result = [slice(*m.span()) for m in regex.finditer(sequence)]
-    if not result:
-        raise IDPConfGenException(f'No matches found for: {regex.pattern}')
-    else:
-        return result
+    regex_c = re.compile(regex)
+    return [slice(*m.span()) for m in regex_c.finditer(sequence)]
 
 
 def regex_forward_with_overlap(sequence, regex):
     """
-    Returns the indexes of re.finditer.
+    Find matches for regex in sequence of chars.
+
+    Considers regex defines overlap.
 
     Accepts only regex expressions with overlap, for example:
 
         r'(?=(L{3}))'
+
+    Returns
+    -------
+    list of slices
+        Where slice.start and slice.stop follow Python conventions.
     """
-    result = [slice(m.start(1), m.end(1)) for m in regex.finditer(sequence)]
-    if not result:  # no match found
-        # I decided to raise an error here because of the functionality of
-        # IDPConfGen. It can lead to hidden bugs and errors if a regex without
-        # matches is given, at least for now
-        raise IDPConfGenException()#errmsg=f'No matches found for: {regex.pattern}')
-    else:
-        return result
+    regex_c = re.compile(regex)
+    return [slice(m.start(1), m.end(1)) for m in regex_c.finditer(sequence)]
