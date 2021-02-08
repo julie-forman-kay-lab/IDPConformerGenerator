860b1135372f356a1d934164e56874b272c94594
joaomcteixeira <joaomcteixeira@gmail.com>
2021-02-08 09:59:57 -0500

    ConfMasks namedtuple

--- src/idpconfgen/cli_build.py
+++ src/idpconfgen/cli_build.py
@@ -26,6 +26,7 @@ from numba import njit
 import idpcpp
 #from idpconfgen.cpp.faspr import faspr_sidechains as fsc
 from idpconfgen import log, Path
+from idpconfgen import assert_type as AT
 from idpconfgen.core.build_definitions import (
     amber_pdbs,
     atom_labels_amber,
@@ -141,15 +142,102 @@ CONF_NUMBER = Queue()
 ConfMasks = namedtuple(
     'ConfMaks',
     [
-        'bb_mask',
-        'bb4_mask',
-        'CO_mask',
-        'NH_mask',
-        'OXT_mask',
+        'bb3',
+        'bb4',
+        'NHs',
+        'COs',
+        'Hterm',
+        'OXT1',
+        'OXT2',
+        'non_Hs',
+        'non_Hs_non_OXT',
+        'H1_N_CA_HA',
         ]
     )
 
+def init_confmasks(atom_labels):
+    """
+    Create a ConfMask object (namedtuple).
 
+    ConfMask is a named tuple which attributes are integer masks for the
+    respective groups.
+
+    Parameters
+    ----------
+    atom_labels : array-like
+        The atom names of the protein.
+
+    Returns
+    -------
+    namedtuple
+
+    Notes
+    -----
+    ConfMask attributes map to the following atom groups:
+
+    bb3 : N, CA, C
+    bb4 : N, CA, C, O
+    NHs : amide protons
+    Hterm : N-terminal protons
+    OXT1 : O atom of C-terminal carboxyl group
+    OXT2 : OXT atom of the C-terminal carboxyl group
+    non_Hs : all but hydrogens
+    non_Hs_non_OXT : all but hydrogens and the only OXT atom
+    H1_N_CA_HA : these four atoms from the first residue
+    """
+    bb3 = np.where(np.isin(atom_labels, ('N', 'CA', 'C')))[0]
+    assert len(bb3) % 3 == 0
+
+    bb4 = np.where(np.isin(atom_labels, ('N', 'CA', 'C', 'O')))[0]
+    assert len(bb4) % 4 == 0
+
+    NHs = np.where(atom_labels == 'H')[0]
+
+    # last O belongs to the C-term carboxyl, we don't want it in the carbonyl
+    # mask
+    COs = np.where(atom_labels == 'O')[0][:-1]
+
+    OXT1 = np.where(atom_labels == 'O')[0][-1]
+    # the actual OXT atom, this is only one O of the C-term carboxyl pair
+    OXT2 = np.where(atom_labels == 'OXT')[0][0]  # int instead of list
+
+    rr = re.compile(r'H+')
+    hs_match = np.vectorize(lambda x: bool(rr.match(x)))
+    non_Hs = np.where(np.logical_not(hs_match(atom_labels)))[0]
+    non_Hs_non_OXT = non_Hs[:-1]
+
+    # used to rotate the N-terminal Hs to -60 degrees to  HA during
+    # the building process
+    _H1_idx = np.where(atom_labels == 'H1')[0]
+    assert len(_H1_idx) == 1
+
+    # of the first residue
+    _N_CA_idx = np.where(np.isin(atom_labels, ('N', 'CA')))[0][:-2]
+    assert len(_N_CA_idx) == 2
+
+    _HA_HA2_idx = np.where(np.isin(atom_labels, ('HA', 'HA2')))[0][0:1]
+    assert len(_HA_HA2_idx) == 1
+
+    H1_N_CA_HA = list(_H1_idx) + list(_N_CA_idx) + list(_HA_HA2_idx)
+    assert len(H1_N_CA_HA) == 4
+
+    Hterm = np.where(np.isin(atom_labels, ('H1', 'H2', 'H3')))[0]
+    assert len(Hterm) == 3
+
+    conf_mask = ConfMasks(
+        bb3=bb3,
+        bb4=bb4,
+        NHs=NHs,
+        COs=COs,
+        Hterm=Hterm,
+        OXT1=OXT1,
+        OXT2=OXT2,
+        non_Hs=non_Hs,
+        non_Hs_non_OXT=non_Hs_non_OXT,
+        H1_N_CA_HA=H1_N_CA_HA,
+        )
+
+    return conf_mask
 
 
 
