48059c3b670d49aaa7ed3a02d1e64958f4d48c31
joaomcteixeira <joaomcteixeira@users.noreply.github.com>
2021-05-28 18:34:08 +0000

    [SKIP] version bump 0.0.11 -> 0.0.12
    Merge pull request #116
    
    Corrects suffix inspection in cli_torsions

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ long_description = '{}\n{}'.format(
 
 setup(
     name='idpconfgen',
-    version='0.0.11',
+    version='0.0.12',
     description='Generates IDP conformers.',
     long_description=long_description,
     author='Julie Forman-Kay Lab',
