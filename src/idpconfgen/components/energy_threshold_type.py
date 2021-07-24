a2f85d403cd476ce84a6a7abeb5d60b22a2ccd34
joaomcteixeira <joaomcteixeira@gmail.com>
2021-07-24 21:10:00 +0200

    updates help

--- src/idpconfgen/components/energy_threshold_type.py
+++ src/idpconfgen/components/energy_threshold_type.py
@@ -6,13 +6,17 @@ from idpconfgen.libs.libenergyij import post_calc_options
 
 
 et_type_help = \
-"""How to evaluate the energy threshold. `whole` evaluates threshold against the
-sum of all contributions. `pairs` evaluates threshold against the pair of
-highest energy. If you use `pairs` define a minimum -etbb and -etss of 2 or 3,
-depending on the system. This is because pairs at 1-4 (three bonds apart) will
-always have a small positive energy contribution. If you choose `whole`, you can
-even defined negative numbers for -etbb and -etss.
-"""
+"""How to calculate the energy for 'ij' atom pairs. `whole` calculates the
+energy as a single float for the whole conformer (performs sum of pair
+contributions. `pairs` calculates energy for individual pairs. In the current
+implementation, the energy threshold (-etbb and -etss) will be compared to the
+`whole` calculation or to the individual `pairs`. In `pairs`, if a single pair
+has energy above threshold the conformer is discarded. If you use `pairs`, the
+minimum values for `-etbb` and `-etss` should not be bellow 2 or 3 because pairs
+at 1-4 (three bonds apart) will always have a small positive energy
+contribution.  Hence, setting `-etbb` or `-etss` to 0 with `pairs` will result
+in infinite calculations. If you choose `whole`, you can even define negative
+numbers for `-etbb` and `-etss`."""
 
 et_type_args = ['-et', '--et-type']
 et_type_kwargs = {
