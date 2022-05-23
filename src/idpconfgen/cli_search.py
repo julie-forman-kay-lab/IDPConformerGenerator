90ddf9a77bc1052e123fbb348613c69c32ddaab3
joaomcteixeira <joaomcteixeira@gmail.com>
2022-05-23 10:19:36 +0200

    minors and changelog

--- src/idpconfgen/cli_search.py
+++ src/idpconfgen/cli_search.py
@@ -3,20 +3,21 @@ Calculate number of hits of keywords in the header from raw PDB files fetched.
 
 USAGE:
     $ idpconfgen search -fpdb <PDB-FILES> -kw <KEYWORDS>
-    $ idpconfgen search -fpdb <PDB-FILES> -kw <KEYWORDS> -o <OUTPUT> -n <CORES>    
+    $ idpconfgen search -fpdb <PDB-FILES> -kw <KEYWORDS> -o <OUTPUT> -n <CORES>
 """
 import argparse, os, json, shutil
-from pathlib import Path
 from functools import partial
+from pathlib import Path
 
+from idpconfgen import log
 from idpconfgen.libs.libio import (
     extract_from_tar,
     read_path_bundle,
-)
-from idpconfgen import log
+    )
 from idpconfgen.libs import libcli
 from idpconfgen.libs.libmulticore import pool_function
 from idpconfgen.logger import S, T, init_files, report_on_crash
+
 
 LOGFILESNAME = '.search'
 _name = 'search'
@@ -39,9 +40,9 @@ ap.add_argument(
     help=(
         "Keywords to look for in the header of PDB files. "
         "Delimited by commas, e.g. -kw 'pro,beta,elastin'"
-    ),
+        ),
     nargs='?',
-)
+    )
 
 ap.add_argument(
     '-o',
@@ -69,19 +70,21 @@ ap.add_argument(
 
 libcli.add_argument_ncores(ap)
 
-def find_in_header(kwds, file):
-    with open(file) as r:
+
+def find_in_header(kwds, file_):
+    with open(file_) as r:
         line = r.readline().lower()
         if "header" not in line:
             return None
         while "expdta" not in line:
             for word in kwds:
                 if word in line:
-                    return word, file
-            
+                    return word, file_
+
             line=r.readline().lower()
-            
+
     return None
+
 
 def main(
         pdb_files,
@@ -90,41 +93,43 @@ def main(
         tmpdir=TMPDIR,
         ncores=1,
         **kwargs,
-    ):
+        ):
     """
     Performs main client logic.
-    
-    Searches through raw PDB files' headers for certain 
-    keywords and return the PDB IDs that had hits to the keywords.  
+
+    Searches through raw PDB files' headers for certain
+    keywords and return the PDB IDs that had hits to the keywords.
 
     Parameters
-    ----------        
+    ----------
     pdb_files : str or Path
         Path to a .TAR or folder of PDB files.
         Required for `--search`.
-    
+
     keywords : str
         String of keywords delimited by commas to search for.
         Required for `--search`.
-    
+
     output (str, optional): str or Path
         Path to the file to store the number of search hits.
         Defaults to "search_hits.json". Optional for `--search`.
-        
+
     tmpdir : str or Path
         Path to the temporary directory if working with .TAR files.
         Defaults to TMPDIR. Optional for `--search`.
-    
+
     ncores : int
         The number of cores to use.
         Defaults to 1. Optional for `--search`.
     """
     init_files(log, LOGFILESNAME)
-    
+
     if not pdb_files or not keywords:
-        log.info(S('To use the search function you must provide PDB files and a list of keywords.'))
+        log.info(S(
+            'To use the search function you must provide PDB files '
+            'and a list of keywords.'))
         return
-    
+
     log.info(T('reading input paths'))
     try:
         pdbs2operate = extract_from_tar(pdb_files, output=tmpdir, ext='.pdb')
@@ -133,46 +138,47 @@ def main(
         pdbs2operate = list(read_path_bundle(pdb_files, ext='pdb'))
         _istarfile = False
     log.info(S('done'))
-    
+
     log.info(T('preparing task execution'))
     try:
         keywords = keywords.split(",")
     except:
         log.info(S('ERROR: keywords must be delimited by commas.'))
         return
-    
+
     execute = partial(
         report_on_crash,
         find_in_header,
         keywords,
         )
     execute_pool=pool_function(execute, pdbs2operate, ncores=ncores)
-    
+
     matches={}
     for word in keywords:
         matches[word]=[]
-        
+
     for result in execute_pool:
         if result != None:
             wd = result[0]
             id = os.path.basename(result[1])
             matches[wd].append(id)
-    
+
     for wd in keywords:
         log.info(S(f'total of {len(matches[wd])} instances of {wd}.'))
-    
+
     log.info(S('done'))
-    
+
     log.info(T('writing matches to disk'))
     _output = json.dumps(matches, indent = 4)
     with open(output, mode="w") as fout:
         fout.write(_output)
     log.info(S('done'))
-    
+
     if _istarfile:
         shutil.rmtree(tmpdir)
-        
+
     return
+
 
 if __name__ == '__main__':
     libcli.maincli(ap, main)
