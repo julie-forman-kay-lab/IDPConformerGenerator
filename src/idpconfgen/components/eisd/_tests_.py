cd95c6dc7c1f472712093e4995596fbc9fc1d036
menoliu <nemoliu123@gmail.com>
2022-07-13 13:14:03 -0400

    fret calculator

--- src/idpconfgen/components/eisd/_tests_.py
+++ src/idpconfgen/components/eisd/_tests_.py
@@ -2,16 +2,30 @@
 Temporary python script used for testing functions.
 Will be deleted upon integration completion.
 """
+import numpy as np
 
+from pathlib import Path
 from Bio.PDB import PDBParser as parser
-from idpconfgen.libs.libstructure import Structure
+from idpconfgen.libs.libstructure import (
+    Structure,
+    col_name,
+)
+
+
+def get_scalar(x, y, z):
+    return (x**2 + y**2 + z**2)**0.5
 
 pdb = "/home/nemoliu/Documents/eisdTest/asyn_CSSS_nosub/conformer_2_mcsce.pdb"
 p = parser()
 struct = p.get_structure(id='p', file=pdb)
-print(struct[0]['A'][1]['CA'] - struct[0]['A'][2]['CA'])
+print(struct[0]['A'][22]['CA'] - struct[0]['A'][23]['CA'])
 
 # jul 12, error, doesn't recognize MC-SCE PDB files
-s = Structure(pdb)
+path_pdb = Path(pdb)
+s = Structure(path_pdb)
 s.build()
-print(s[0]['A'][1]['CA'] - s[0]['A'][2]['CA'])
+s.add_filter(lambda x: x[col_name] == 'CA')
+da = s.coords[22, :] - s.coords[20, :]
+assert da.shape == (3,)
+dist = get_scalar(da[0], da[1], da[2])
+print(dist)
--- src/idpconfgen/components/eisd/calculators.py
+++ src/idpconfgen/components/eisd/calculators.py
@@ -7,12 +7,17 @@ Partially inspired from: https://github.com/Oufan75/X-EISD/blob/master/EISD_back
 import numpy as np
 import pandas as pd
 
-from idpconfgen.libs.libstructure import Structure
+from idpconfgen.libs.libstructure import Structure, col_name
 from idpconfgen.libs.libcalc import calc_torsion_angles
 
 # chemical shift back calculations using UCBShifts (Li et al. 2020
 # https://github.com/THGLab/CSpred.git); reading outputs
 UCBSHIFT_MAP = {'H':0, 'HA':1, 'C':2, 'CA':3, 'CB':4, 'N':5}
+
+
+def get_scalar(x, y, z):
+    return (x ** 2 + y ** 2 + z ** 2) ** 0.5
+
 
 def fret_calculator(exp_file, struct_path):
     fret_bc = []
@@ -26,12 +31,15 @@ def fret_calculator(exp_file, struct_path):
     
     s = Structure(struct_path)
     s.build()
+    s.add_filter(lambda x: x[col_name] == 'CA')
     #assumes CA as atom labeled
     for j in range(exp.shape[0]):
         r1 = np.int(res1[j])
         r2 = np.int(res2[j])
         # distance between 2 CA atoms
-        d = s[0]['A'][r1]['CA'] - s[0]['A'][r2]['CA'] 
+        _d = s.coords[r1, :] - s.coords[r2, :]
+        assert _d.shape == (3,)
+        d = get_scalar(_d[0], _d[1], _d[2])
         
         # scale_factor to adjust for dye size and CA to label distances
         scale_factor = ((np.abs(r1 - r2) + 7) / np.abs(r1 - r2)) ** 0.5
@@ -42,3 +50,4 @@ def fret_calculator(exp_file, struct_path):
     
     fret_bc = np.reshape(fret_bc, (-1, exp.shape[0]))
     return fret_bc
+
