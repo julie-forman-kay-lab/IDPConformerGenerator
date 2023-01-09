d3940100d543307e8c9e98e0bf92aa1dd3042ad7
Nemo (Zi Hao) Liu <nemoliu123@gmail.com>
2023-01-09 17:48:34 -0500

    finish resptm subclient

--- src/idpconfgen/cli_resptm.py
+++ src/idpconfgen/cli_resptm.py
@@ -17,6 +17,13 @@ from idpconfgen.libs.libio import extract_from_tar, read_path_bundle
 from idpconfgen.libs.libmulticore import pool_function
 from idpconfgen.logger import S, T, init_files, report_on_crash
 
+from idpconfgen.libs.libio import make_folder_or_cwd
+from idpconfgen.libs.libstructure import (
+    Structure,
+    structure_to_pdb,
+    col_resName,
+    col_resSeq,
+)
 
 LOGFILESNAME = '.idpconfgen_resptm'
 TMPDIR = '__tmpresptm__'
@@ -39,24 +46,14 @@ ap.add_argument(
     '-pt',
     '--pattern',
     help=(
-        "Entire pattern in single quotes. Pattern is denoted by colons "
-        "for residue number and desired new residue name. Additional residues "
-        "are delimited by commas and pattern must end at a comma. "
-        "For e.g. -pt '12:TPO,14:SEP,'"
+        "Pattern is denoted by colons for residue number and desired "
+        "new residue name. Additional residues are delimited by commas "
+        "and pattern must end at a comma. For e.g. -pt 12:TPO,14:SEP,"
         ),
     nargs='?',
     )
 
-ap.add_argument(
-    '-o',
-    '--output',
-    help=(
-        "A path to the folder where renamed residue PDBs are stored. "
-        "Defaults to overwrite working files."
-        ),
-    type=Path,
-    )
-
+libcli.add_argument_output_folder(ap)
 libcli.add_argument_ncores(ap)
 
 ap.add_argument(
@@ -69,18 +66,52 @@ ap.add_argument(
     default=TMPDIR,
     )
 
+
+def rename_residues(target_struc, pattern):
+    """
+    Renames 3 letter residue codes in a PDB based on the pattern.
+
+    Parameters
+    ----------
+    target_struc : Path
+        Path to PDB file to edit.
+        
+    pattern : dict of str
+        Dictionary of patterns where the first key-layer is the residue number
+        and values are 3-letter code to be changed.
+    
+    Returns
+    -------
+    new_struc_arr : np.ndarray (N x 16)
+        Array format of new re-named PDB file.
+    """
+    struc = Structure(target_struc)
+    struc.build()
+    
+    new_struc_array = struc.data_array
+    for target_res in pattern:
+        for i, res in enumerate(new_struc_array[:, col_resSeq]):
+            if res == target_res:
+                new_struc_array[:, col_resName][i] = pattern[target_res]
+            elif res == str(int(target_res) + 1):
+                break
+            
+    return new_struc_array
+
+
 def main(
         pdb_files,
         pattern,
-        output,
+        output_folder=None,
         ncores=1,
         tmpdir=TMPDIR,
         **kwargs,
         ):
-    init_files(log, LOGFILESNAME)
-    REGEX_PATTERN = re.compile("[0-9]*:[a-z]{3},")
-    RESNUM = []
-    RESNAME = []
+    output_folder = make_folder_or_cwd(output_folder)
+    init_files(log, Path(output_folder, LOGFILESNAME))
+    
+    REGEX_PATTERN = re.compile(r"[0-9]*:[A-Z]{3},")
+    RES_COMBO = {}
     
     log.info(T('reading input paths'))
     try:
@@ -97,13 +128,33 @@ def main(
         residues.pop()  # last element should be empty
         for res in residues:
             tmp = res.split(':')
-            RESNUM.append(tmp[0])
-            RESNAME.append(tmp[1])
+            RES_COMBO[tmp[0]] = tmp[1]
+            log.info(S(f'Residue {tmp[0]} will be changed to {tmp[1]}.'))
     else:
         log.info(S('Incorrect pattern input.'))
         log.info(S('Pattern is as follows: 1:ABC,...'))
         return
     log.info(S('done'))
+    
+    consume = partial(
+        rename_residues,
+        pattern=RES_COMBO,
+    )
+    execute = partial(
+        report_on_crash,
+        consume,
+        ROC_exception=Exception,
+        ROC_folder=output_folder,
+        ROC_prefix=_name
+    )
+    execute_pool = pool_function(execute, pdbs2operate, ncores=ncores)
+    
+    for i, conf in enumerate(execute_pool):
+        struc = structure_to_pdb(conf)
+        output = output_folder.joinpath(f"resptm_conf_{i + 1}.pdb")
+        with open(output, 'w') as f:
+            for line in struc:
+                f.write(line + "\n")
     
     if _istarfile:
         shutil.rmtree(tmpdir)
