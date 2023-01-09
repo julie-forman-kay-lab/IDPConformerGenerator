919cf3d8a197728bb82f43b46f3c29fbf11d7434
Nemo (Zi Hao) Liu <nemoliu123@gmail.com>
2023-01-09 15:20:47 -0500

    pattern and processing for resptm

--- src/idpconfgen/cli_resptm.py
+++ src/idpconfgen/cli_resptm.py
@@ -6,9 +6,8 @@ USAGE:
     $ idpconfgen resptm <PDB-FILES> -pt <PATTERN> -o <OUTPUT> -n <CORES>
 """
 import argparse
-import json
-import os
 import shutil
+import re
 from functools import partial
 from pathlib import Path
 
@@ -20,6 +19,8 @@ from idpconfgen.logger import S, T, init_files, report_on_crash
 
 
 LOGFILESNAME = '.idpconfgen_resptm'
+TMPDIR = '__tmpresptm__'
+
 _name = 'resptm'
 _help = 'Renames residues of interest within a PDB file for post-translational modifications.'
 
@@ -38,9 +39,10 @@ ap.add_argument(
     '-pt',
     '--pattern',
     help=(
-        "Pattern separated by colons for residue number and desired new "
-        "residue name. Additional residues are delimited by commas, "
-        "e.g. -pt '12:TPO,14:SEP'"
+        "Entire pattern in single quotes. Pattern is denoted by colons "
+        "for residue number and desired new residue name. Additional residues "
+        "are delimited by commas and pattern must end at a comma. "
+        "For e.g. -pt '12:TPO,14:SEP,'"
         ),
     nargs='?',
     )
@@ -57,14 +59,54 @@ ap.add_argument(
 
 libcli.add_argument_ncores(ap)
 
+ap.add_argument(
+    '-tmpdir',
+    help=(
+        'Temporary directory to store data during calculation '
+        'if needed.'
+        ),
+    type=Path,
+    default=TMPDIR,
+    )
+
 def main(
         pdb_files,
         pattern,
         output,
         ncores=1,
+        tmpdir=TMPDIR,
         **kwargs,
         ):
-    pass
+    init_files(log, LOGFILESNAME)
+    REGEX_PATTERN = re.compile("[0-9]*:[a-z]{3},")
+    RESNUM = []
+    RESNAME = []
+    
+    log.info(T('reading input paths'))
+    try:
+        pdbs2operate = extract_from_tar(pdb_files, output=tmpdir, ext='.pdb')
+        _istarfile = True
+    except (OSError, TypeError):
+        pdbs2operate = list(read_path_bundle(pdb_files, ext='pdb'))
+        _istarfile = False
+    log.info(S('done'))
+    
+    log.info(T('reading input pattern'))
+    if REGEX_PATTERN.match(pattern):
+        residues = pattern.split(',')
+        residues.pop()  # last element should be empty
+        for res in residues:
+            tmp = res.split(':')
+            RESNUM.append(tmp[0])
+            RESNAME.append(tmp[1])
+    else:
+        log.info(S('Incorrect pattern input.'))
+        log.info(S('Pattern is as follows: 1:ABC,...'))
+        return
+    log.info(S('done'))
+    
+    if _istarfile:
+        shutil.rmtree(tmpdir)
 
 
 if __name__ == '__main__':
