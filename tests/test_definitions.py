90ae6fec14e3566ebabb34095dfb1dd19203a0de
joaomcteixeira <joaomcteixeira@gmail.com>
2023-07-18 15:21:48 +0200

    added PTMs to test list

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -17,4 +17,4 @@ def test_aa_letter_dictionaries(aadict):
     """Test amino acids letter conversion dictionaries."""
     assert isinstance(aadict, dict)
     # there are 20 natural aminoacids + 3 His protonation states
-    assert len(aadict) == 23
+    assert len(aadict) == 25
